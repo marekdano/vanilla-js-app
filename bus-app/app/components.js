0284cce5ea0abaab3e9e6e676fad1f2da379a562
Tomasz Waraksa <tomasz@waraksa.net>
2019-07-17 09:09:03 +0000

    Minor improvements

--- bus-app/app/components.js
+++ bus-app/app/components.js
@@ -3,4 +3,4 @@ import './components/footer/footer.js'
 import './components/search-box/search-box.js'
 import './components/bus-departures/bus-departures.js'
 
-export { Components, Tags } from './components/component.js'
+export { Components } from './components/component.js'
